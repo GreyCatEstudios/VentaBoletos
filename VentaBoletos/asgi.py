8d9356ea6fcaa4e28a68841950f5fe12dd72172b
GreyCatEstudios <greycatestudios@gmail.com>
2023-10-15 07:11:58 -0600

    Initial commit

---
+++ VentaBoletos/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for VentaBoletos project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.2/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'VentaBoletos.settings')
+
+application = get_asgi_application()
